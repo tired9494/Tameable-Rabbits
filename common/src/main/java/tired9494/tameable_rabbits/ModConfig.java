504b1cb9bc829cf346cb1bc8511a5780155508cb
tired <tired9494@gmail.com>
2026-05-25 16:50:16 +0200

    "implement" resourceful config
    Took 10 minutes

--- common/src/main/java/tired9494/tameable_rabbits/ModConfig.java
+++ common/src/main/java/tired9494/tameable_rabbits/ModConfig.java
@@ -1,13 +1,11 @@
 package tired9494.tameable_rabbits;
 
-import eu.midnightdust.lib.config.MidnightConfig;
+import com.teamresourceful.resourcefulconfig.api.annotations.Config;
+import com.teamresourceful.resourcefulconfig.api.annotations.ConfigEntry;
 
-public class ModConfig extends MidnightConfig {
-    public static final String SPLASHLING = "splashling";
-    public static final String ASHLING = "ashling";
-
-    @Entry(category = SPLASHLING) public static int splashlingBucketCapacity = 8;
-
-    @Entry(category = ASHLING) public static int ashlingBucketCapacity = 4;
-
+@Config(value="tameable_rabbits")
+public class ModConfig {
+    @ConfigEntry(
+            type =
+    )
 }
